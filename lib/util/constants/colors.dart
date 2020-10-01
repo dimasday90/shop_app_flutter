b7261aef36c886777f1cc6d0a1113af8126a894f
Dimas Agung Yudono <dimasday90@gmail.com>
2020-10-01 15:16:00 +0700

    add new constant color

--- lib/util/constants/colors.dart
+++ lib/util/constants/colors.dart
@@ -6,6 +6,7 @@ Color cDeepOrange = Colors.deepOrange;
 Color cOrangeAccent = Colors.orangeAccent;
 Color cGrey = Colors.grey;
 Color bBlack54 = Colors.black54;
+Color sBlack26 = Colors.black26;
 Color cFavorite = Colors.red;
 Color cWhite = Colors.white;
 Color bBlue900 = Colors.blue[900];
